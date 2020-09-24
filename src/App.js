cf53277a23fb0694493b1058b14d5017b9a6003b
KooKu <kooku@kakao.com>
2020-09-24 18:21:33 +0900

    t

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import AutoComplete from './components/auto-complete'
-import './style.css'
 
 function App() {
-  const itemList = ['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번', '9번', '10번']
-
   return (
     <div className="App">
-      <div>
-        <AutoComplete itemList={itemList} />
-      </div>
+      <div>hello</div>
     </div>
   )
 }
