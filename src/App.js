53fb3f458b4f1eb655ca90a59686947c524a1aa3
KooKu <koomg9599@gmail.com>
2020-09-24 17:40:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AutoComplete from './components/auto-complete'
 import './style.css'
 
 function App() {
-  const itemList = ['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번', '9번', '10번']
+  const itemList = ['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번', '9번', '11번']
 
   return (
     <div className="App">
